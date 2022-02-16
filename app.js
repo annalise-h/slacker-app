90b71b5cc5916fb7d94d22ef6477d108ae1c3293
annalise-h <annaliseh93@gmail.com>
2022-02-15 20:36:19 -0500

    add login and register apis

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
-const port = 3000;
+const { Server } = require("socket.io");
+const routes = require("./routes/routes");
 
 const app = express();
+
+// set public view directory
+app.use(express.static(path.join(__dirname, "public")));
 
 // express.json() is a method inbuilt in express to recognize the incoming Request Object as a JSON Object
 app.use(express.json());
@@ -10,8 +15,24 @@ app.use(express.json());
 // this also will read html forms
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/", routes);
+
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(port);
+const server = http.createServer(app);
+// io will be our websocket server that integrates with the Node.JS HTTP Server
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  // listen for whenever we receive a "message-sent" event from the client
+  socket.on("message-sent", (msg) => {
+    // send a message back to all clients connected to the server
+    io.emit("message-received", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
