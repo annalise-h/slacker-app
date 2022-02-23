41b750845c319304df6a756ed948f4caead88c60
annalise-h <annaliseh93@gmail.com>
2022-02-22 19:14:28 -0500

    create chatroom routes, migrage html to ejs

--- public/scripts.js
+++ public/scripts.js
@@ -1,13 +1,31 @@
 // io() function will give us an individual socket
-const socket = io();
+const socket = io({
+  autoConnect: false,
+});
 
-socket.on("connect", () => {
-  console.log(`You connected with id ${socket.id}`);
+$(document).ready(() => {
+  const sessionID = localStorage.getItem("sessionID");
+  if (sessionID) {
+    socket.auth = { sessionID };
+    socket.connect();
+  }
+});
+
+socket.on("session", ({ sessionID, userID, username }) => {
+  // attach the session ID to the next reconnection attempts
+  socket.auth = { sessionID };
+  // store it in the localStorage
+  localStorage.setItem("sessionID", sessionID);
+  // save the ID and username of the user
+  socket.userID = userID;
+  socket.username = username;
 });
 
 // listen for when our server receives a message from the socket
 socket.on("message-received", (msg) => {
-  $("#messages").append(`<li> ${msg} </li>`);
+  $("#messages").append(`
+  <p> ${socket.username}: <p>
+  <li> ${msg} </li>`);
   $("#input").val("");
 });
 
